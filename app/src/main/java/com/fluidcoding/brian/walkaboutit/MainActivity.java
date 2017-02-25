2f48a362d308e195be66978c14ef5622bcd15616
= <=>
2017-02-25 13:05:45 -0500

    Button

--- app/src/main/java/com/fluidcoding/brian/walkaboutit/MainActivity.java
+++ app/src/main/java/com/fluidcoding/brian/walkaboutit/MainActivity.java
@@ -1,13 +1,30 @@
 package com.fluidcoding.brian.walkaboutit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_startWalking;
+    Intent locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = new Intent(this.getActivity(), LocationSerive.class);
+
+        getActivity().startService();
+
+        btn_startWalking = (Button)findViewById(R.id.btn_startWalking);
+        btn_startWalking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
