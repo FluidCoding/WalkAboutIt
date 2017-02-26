753be8790fc06692710ddaf6e77a0723a43b73e7
= <=>
2017-02-26 08:13:29 -0500

    geofence shit

--- app/src/main/java/com/fluidcoding/brian/walkaboutit/LocationService.java
+++ app/src/main/java/com/fluidcoding/brian/walkaboutit/LocationService.java
@@ -1,8 +1,20 @@
 package com.fluidcoding.brian.walkaboutit;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.content.Context;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -16,6 +28,7 @@ public class LocationService extends IntentService {
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     private static final String ACTION_FOO = "com.fluidcoding.brian.walkaboutit.action.FOO";
     private static final String ACTION_BAZ = "com.fluidcoding.brian.walkaboutit.action.BAZ";
+    private final String TAG = "locationService";
 
     // TODO: Rename parameters
     private static final String EXTRA_PARAM1 = "com.fluidcoding.brian.walkaboutit.extra.PARAM1";
@@ -57,19 +70,109 @@ public class LocationService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        if (intent != null) {
-            final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
-            }
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()) {
+            //String errorMessage = GeofenceErrorMessages.getErrorString(this,
+            //        geofencingEvent.getErrorCode());
+            //Log.e(TAG, errorMessage);
+            return;
+        }
+
+        // Get the transition type.
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        // Test that the reported transition was of interest.
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER)
+        {
+
+            // Get the geofences that were triggered. A single event can trigger
+            // multiple geofences.
+            List triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+
+             //Get the transition details as a String.
+            String geofenceTransitionDetails = getGeofenceTransitionDetails(
+                    this,
+                    geofenceTransition,
+                    triggeringGeofences
+            );
+
+            // Send notification and log the transition details.
+            //sendNotification(geofenceTransitionDetails);
+            Log.i(TAG, geofenceTransitionDetails);
+        }
+        else
+        {
+            // Log the error.
+            //Log.e(TAG, getString(R.string.geofence_transition_invalid_type,
+            //        geofenceTransition));
+        }
+
+    }
+
+    private String getGeofenceTransitionDetails(
+            Context context,
+            int geofenceTransition,
+            List<Geofence> triggeringGeofences) {
+
+        String geofenceTransitionString = getTransitionString(geofenceTransition);
+
+        // Get the Ids of each geofence that was triggered.
+        ArrayList triggeringGeofencesIdsList = new ArrayList();
+        for (Geofence geofence : triggeringGeofences) {
+            triggeringGeofencesIdsList.add(geofence.getRequestId());
+        }
+        String triggeringGeofencesIdsString = TextUtils.join(", ", triggeringGeofencesIdsList);
+
+        return geofenceTransitionString + ": " + triggeringGeofencesIdsString;
+    }
+
+    private String getTransitionString(int transitionType) {
+        switch (transitionType) {
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                return "transition - enter";
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                return "transition - exit";
+            default:
+                return "unknown transition";
         }
     }
+
+    private void sendNotification(String notificationDetails) {
+        // Create an explicit content Intent that starts the main Activity.
+        Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+        // Construct a task stack.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+        // Add the main Activity to the task stack as the parent.
+        stackBuilder.addParentStack(MainActivity.class);
+
+        // Push the content Intent onto the stack.
+        stackBuilder.addNextIntent(notificationIntent);
+
+        // Get a PendingIntent containing the entire back stack.
+        PendingIntent notificationPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // Get a notification builder that's compatible with platform versions >= 4
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        // Define the notification settings.
+        builder.setContentTitle(notificationDetails)
+                .setContentText("Click notification to return to app")
+                .setContentIntent(notificationPendingIntent);
+
+        // Dismiss notification once the user touches it.
+        builder.setAutoCancel(true);
+
+        // Get an instance of the Notification manager
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Issue the notification
+        mNotificationManager.notify(0, builder.build());
+    }
+
 
     /**
      * Handle action Foo in the provided background thread with the provided
--- app/src/main/java/com/fluidcoding/brian/walkaboutit/MapActivity.java
+++ app/src/main/java/com/fluidcoding/brian/walkaboutit/MapActivity.java
@@ -1,7 +1,9 @@
 package com.fluidcoding.brian.walkaboutit;
 
 import android.annotation.TargetApi;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -26,7 +28,10 @@ import android.widget.SpinnerAdapter;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -53,8 +58,7 @@ import java.util.ArrayList;
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        LocationListener
-{
+        LocationListener{
 
     Button btnStartWalk;
     Button btnStopWalk;
@@ -67,12 +71,15 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     private LocationManager locmn, locmg;
     private ArrayList<Geotag> listOfGs;
     private Location yourloc, dest;
-    private Marker mark;
+    private Marker mark, mark2;
     private Spinner spin;
     private Geofence gf;
+    static int p_points = 0;
     //private MapView vMap;
     GoogleApiClient mGoogleApiClient = null;
-    public void onProviderEnabled(String provider){}
+
+    public void onProviderEnabled(String provider) {
+    }
 
     @Override
     public void onProviderDisabled(String provider) {
@@ -80,10 +87,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
 
-    public void onLocationChanged(Location location){
-
+    public void onLocationChanged(Location location) {
         if(mark != null)
-            mMap.clear();
+        mark.remove();
 
         mark = mMap.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("Your Location"));
 
@@ -91,7 +97,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.zoomTo(19.0f));
         mMap.animateCamera(CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
 
-
+        btnStartWalk.setEnabled(true);
+        yourloc = location;
     }
 
     @Override
@@ -99,12 +106,30 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     }
 
+    PendingIntent pIntent;
+
+//    private PendingIntent getGeofencePendingIntent() {
+//        // Reuse the PendingIntent if we already have it.
+//        if (mGeofencePendingIntent != null) {
+//            return mGeofencePendingIntent;
+//        }
+//        Intent intent = new Intent(this, GeofenceTransitionsIntentService.class);
+//        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+//        // addGeofences() and removeGeofences().
+//        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) throws SecurityException{
+    protected void onCreate(Bundle savedInstanceState) throws SecurityException {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+        Intent intent = new Intent(this, LocationService.class);
+        pIntent = PendingIntent.getService(this, 0, intent, 0);
+
+
         setSupportActionBar(toolbar);
         SupportMapFragment mf = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mf.getMapAsync(this);
@@ -169,17 +194,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Log.d("FireBase", "firebase init");
                 // Get Post object and use the values to update the UI
-                for(DataSnapshot d : dataSnapshot.getChildren()){
+                for (DataSnapshot d : dataSnapshot.getChildren()) {
                     listOfGs.add(d.getValue(Geotag.class));
                     Log.d(TAG, listOfGs.get(0).name);
                 }
 
                 ArrayList<String> strs = new ArrayList<String>();
-                strs.add("All");
-                if(mMap != null)
-                {
-                    for(Geotag gg:listOfGs)
-                    strs.add(gg.getName());
+                //strs.add("All");
+                if (mMap != null) {
+                    for (Geotag gg : listOfGs)
+                        strs.add(gg.getName());
 
                 }
                 ArrayAdapter<String> stradp = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, strs);
@@ -201,8 +225,8 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         // -----------------------------
         // UI Init/Events
         spin = (Spinner) findViewById(R.id.spinner);
-        btnStartWalk = (Button)findViewById(R.id.btnStart);
-        btnStopWalk = (Button)findViewById(R.id.btnStop);
+        btnStartWalk = (Button) findViewById(R.id.btnStart);
+        btnStopWalk = (Button) findViewById(R.id.btnStop);
 
         btnStartWalk.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -211,16 +235,53 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
                 Geotag selectedTag = listOfGs.get(spin.getSelectedItemPosition());
 
-                mark = mMap.addMarker(new MarkerOptions().position(new LatLng(selectedTag.getLat(), selectedTag.getLng())).
+                mark2 = mMap.addMarker(new MarkerOptions().position(new LatLng(selectedTag.getLat(), selectedTag.getLng())).
                         title(selectedTag.getName()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
 
-                // TODO: Geofencing
-                dest = new Location(yourloc);
+                // TODO: Points
+                dest = new Location("");
                 dest.setLatitude(selectedTag.getLat());
                 dest.setLongitude(selectedTag.getLng());
                 float distance = yourloc.distanceTo(dest);
-                int p_points = (int)distance / 100;
-                Snackbar.make(v, "Account Creation Error", Snackbar.LENGTH_INDEFINITE).show();
+
+                p_points = (int) distance / 10 + 1;
+                //Snackbar.make(v, "Possible Points: " + p_points, Snackbar.LENGTH_LONG).show();
+                Snackbar.make(v, "Distance to goal: " + distance, Snackbar.LENGTH_LONG).show();
+
+                Geofence.Builder buildFence = new Geofence.Builder();
+                Geofence mGeo = buildFence.setRequestId("1")
+                        .setCircularRegion(dest.getLatitude(), dest.getLongitude(), 100.0f)
+                        .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+                        .setExpirationDuration(10000)
+                        .build();
+
+
+                ArrayList<Geofence> geofencingList = new ArrayList<Geofence>();
+                geofencingList.add(mGeo);
+                GoogleApiClient g;
+
+
+                try {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    LocationServices.GeofencingApi.addGeofences(
+                            mGoogleApiClient,
+                            getGeofencingRequest(geofencingList),
+                            pIntent
+                    );
+            }catch(Exception e){
+                    Log.d(TAG, e.toString());
+                }
+
+
 
                 // TODO: Start Collecting Points
             }
@@ -234,12 +295,28 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         });
     }
 
+
     @Override
     protected void onStart() throws SecurityException {
         mGoogleApiClient.connect();
         super.onStart();
         locmn.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 5, this);
         locmg.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5, this);
+    }
+
+    private GeofencingRequest getGeofencingRequest(ArrayList<Geofence> geofenceList) {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+
+        // The INITIAL_TRIGGER_ENTER flag indicates that geofencing service should trigger a
+        // GEOFENCE_TRANSITION_ENTER notification when the geofence is added and if the device
+        // is already inside that geofence.
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+
+        // Add the geofences to be monitored by geofencing service.
+        builder.addGeofences(geofenceList);
+
+        // Return a GeofencingRequest.
+        return builder.build();
     }
 
     @Override
